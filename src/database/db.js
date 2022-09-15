e8e8b0028330219553d718848bac5e3779be8027
Francksuel <francksuelbarbosa@gmail.com>
2022-09-15 15:56:59 -0300

    Testing connection

--- src/database/db.js
+++ src/database/db.js
@@ -4,7 +4,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
-await mongoClient.connect();
 const mongo = async () => {
 	let db;
 	try {
