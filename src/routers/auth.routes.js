df37c951ec82444e79fae84db55e940965d36bfe
Francksuel <francksuelbarbosa@gmail.com>
2022-09-15 16:24:08 -0300

    Testing connection

--- src/routers/auth.routes.js
+++ src/routers/auth.routes.js
@@ -9,8 +9,8 @@ authRouter.post("/sign-up", userValidationSchema, signUp);
 
 authRouter.post("/sign-in", signInValidationSchema, signIn);
 
-authRouter.get("/",(req, res)=>{    
-res.send("hello");
-  })
+authRouter.get("/", (req, res) => {
+	res.send("hello");
+});
 
 export { authRouter };
