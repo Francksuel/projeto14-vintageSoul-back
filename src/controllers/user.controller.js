f4534cf39c49a33177701da81a9228debd6fb55b
MacenaAndre <andremacenacosta@gmail.com>
2022-09-19 13:52:39 -0300

    Validation Address

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,12 +1,27 @@
 import { mongo } from "../database/db.js";
+import joi from "joi";
 
 const db = await mongo();
 
+const addressSchema = joi.object({
+    state: joi.string().min(1).required(),
+    city: joi.string().min(1).required(),
+    district: joi.string().min(1).required(),
+    street: joi.string().min(1).required(),
+    number: joi.number().integer().positive().required(),
+    complement: joi.string().min(1).required()
+});
+
 const editUserAdress = async (req, res) => {
     const userId = res.locals.userId;
+    const address = req.body;
+    const validation = addressSchema.validate(address, {abortEarly: false});
 
-    //falta validação do body
-    
+    if(validation.error) {
+        const errors = validation.error.details.map((error) => error.message).join(" & ");
+        return res.status(422).send(errors);
+    };
+
     try {
         await db.collection("users").updateOne({_id: userId}, {$set: {address: req.body}});
         const user = await db.collection("users").findOne({_id: userId});
